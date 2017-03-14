e51e984cfc3fd1e9859f29b5a6179664834f43e7
ShivonQ <realitykeeper@gmail.com>
2017-03-14 15:24:39 -0500

    Modified Seeds.rb and rover_manifest_serializer.rb

--- app/serializers/rover_manifest_serializer.rb
+++ app/serializers/rover_manifest_serializer.rb
@@ -1,3 +1,3 @@
 class RoverManifestSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id,:name, :landingDate,:launchDate,:status,:maxSol,:maxDate, :portrait, :totalPhotos
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }  { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke'  movie: movies.first)
-rov1 = RoverManifest.create(name:'Spirit', landing_date:'2004-01-04',  launch_date:'2003-06-10',  max_date:'2010-03-21',  status:'complete',  total_photos:124550)
-rov2 = RoverManifest.create(name:'Opportunity', landing_date:'2004-01-25', launch_date:'2003-07-07', max_date:'2017-02-22', status:'active', total_photos:187093)
-rov3 = RoverManifest.create(name:'Curiosity', landing_date:'2012-08-06', launch_date:'2011-11-26', max_date:'2017-03-12', status:'active', total_photos:308239)
+rov1 = RoverManifest.create(name:'Spirit', landing_date:'2004-01-04',  launch_date:'2003-06-10',  max_date:'2010-03-21', portrait:'spirit.jpg', status:'complete',  total_photos:124550)
+rov2 = RoverManifest.create(name:'Opportunity', landing_date:'2004-01-25', launch_date:'2003-07-07', max_date:'2017-02-22', portrait:'opportunity.jpg',status:'active', total_photos:187093)
+rov3 = RoverManifest.create(name:'Curiosity', landing_date:'2012-08-06', launch_date:'2011-11-26', max_date:'2017-03-12', portrait:'curiosity.jpg',status:'active', total_photos:308239)
 
 user1 = User.create(name:'malcolm123', password:'badpassword1')
 user2 = User.create(name:'leehan456', password:'worsepassword2')
