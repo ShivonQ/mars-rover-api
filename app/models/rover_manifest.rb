c04cb20249d0095b4f98b348b97006972abb8813
ShivonQ <realitykeeper@gmail.com>
2017-03-15 11:55:13 -0500

    Finished Serializers & Models?

--- app/models/rover_manifest.rb
+++ app/models/rover_manifest.rb
@@ -1,2 +1,4 @@
 class RoverManifest < ApplicationRecord
+
+  # :id,:name, :landingDate,:launchDate,:status,:maxSol,:maxDate, :portrait, :totalPhotos  [SERIALIZER]
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,4 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :name
+  has_many :fave_imgs
 #   :password, I really doubt I am supposed to be sending this data back in the API
 end
